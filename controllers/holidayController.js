93f2be46d8ca533c454fd838f054af59b2c4e375
alanlinsanity <alan.linyh@gmail.com>
2022-04-02 11:42:05 +0800

    show seed json

--- controllers/holidayController.js
+++ controllers/holidayController.js
@@ -4,17 +4,19 @@ const Holiday = require("../models/Holiday")
 const router = express.Router();
 
 router.get("/seed", async (req, res) => {
-  await Holiday.deleteMany({});
-  await Holiday.insertMany([
-    {
-      name: "New Year's Day",
-    },
-    {
-      name: "Good Friday",
-    }
-  ])
-  res.send("holidays seeded")
-})
+
+    const holidays = [
+      {
+        name: "New Year's Day",
+      },
+      {
+        name: "Good Friday",
+      }
+    ]
+    await Holiday.deleteMany({});
+    await Holiday.insertMany(holidays)
+    res.json(holidays)
+  })
 
 router.get('/', (req, res) => {
   res.send('holidays');
