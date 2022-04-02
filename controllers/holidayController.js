af3f7114c3ee27d4b87c1244c268aa635c29969a
alanlinsanity <alan.linyh@gmail.com>
2022-04-02 15:27:15 +0800

    added cors

--- controllers/holidayController.js
+++ controllers/holidayController.js
@@ -5,21 +5,32 @@ const router = express.Router();
 
 router.get("/seed", async (req, res) => {
 
-    const holidays = [
-      {
-        name: "New Year's Day",
-      },
-      {
-        name: "Good Friday",
-      }
-    ]
-    await Holiday.deleteMany({});
-    await Holiday.insertMany(holidays)
-    res.json(holidays)
-  })
+  const holidays = [
+    {
+      name: "New Year's Day",
+    },
+    {
+      name: "Good Friday",
+    }
+  ]
+  await Holiday.deleteMany({});
+  await Holiday.insertMany(holidays)
+  res.json(holidays)
+})
 
 router.get('/', (req, res) => {
   res.send('holidays');
 })
+
+//* Create Route
+router.post("/", async (req, res) => {
+  console.log("body", req.body)
+  try {
+    const createdHoliday = await Holiday.create(req.body);
+    res.status(200).send(createdHoliday);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  };
+});
 
 module.exports = router;
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const express = require("express");
+const cors = require("cors");
 const mongoose = require("mongoose");
 const HolidayController = require("./controllers/holidayController")
 
@@ -22,6 +23,8 @@ mongoose.connection.once("open", () => {
   console.log("connected to mongoose...");
 });
 
+app.use(cors());
+app.use(express.json());
 app.use("/api/holidays", HolidayController)
 
 
